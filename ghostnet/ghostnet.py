c6ae3e945df2d1fe824d2e463ae174b77cbbfd80
ZhengJianwei2 <zjw@zjut.edu.cn>
2021-04-01 16:23:12 +0800

    Update ghostnet.py

--- ghostnet/ghostnet.py
+++ ghostnet/ghostnet.py
@@ -96,21 +96,7 @@ def conv2d_layer(
 
     return output
 
-    
-# class ConvBnAct(nn.Module):
-#     def __init__(self, in_chs, out_chs, kernel_size,
-#                  stride=1, act_layer=nn.ReLU):
-#         super(ConvBnAct, self).__init__()
-#         self.conv = nn.Conv2d(in_chs, out_chs, kernel_size, stride, kernel_size//2, bias=False)
-#         self.bn1 = nn.BatchNorm2d(out_chs)
-#         self.act1 = act_layer(inplace=True)
-
-#     def forward(self, x):
-#         x = self.conv(x)
-#         x = self.bn1(x)
-#         x = self.act1(x)
-#         return x
-    
+ 
 def conv2d_layer_with_bn(
     name,
     inputs,
